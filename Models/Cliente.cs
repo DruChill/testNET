f869c9d71cdeb88ce225412c268ec2722b1e0bb6
druchill <73948926@elp.edu.pe>
2025-05-24 11:11:39 -0500

    Refactor: Update Cliente model to include Email, Telefono, and Direccion properties

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -4,6 +4,8 @@ namespace MiCrudApi.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
     }
 }
